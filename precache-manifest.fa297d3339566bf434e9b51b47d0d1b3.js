aac314238961673181568ab30d25068642dc4dc7
Bogdan Adrian <bogdan4adrian@gmail.com>
2019-11-18 11:19:14 +0100

    Updates

---
+++ precache-manifest.fa297d3339566bf434e9b51b47d0d1b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c8f7c47b771f995fc92552dccd2080",
+    "url": "/first-react-app-basics/index.html"
+  },
+  {
+    "revision": "b0c1f0294c38411fd81c",
+    "url": "/first-react-app-basics/static/css/main.7f30a24e.chunk.css"
+  },
+  {
+    "revision": "6827eb3341137ecb15ce",
+    "url": "/first-react-app-basics/static/js/2.09a6f05e.chunk.js"
+  },
+  {
+    "revision": "b0c1f0294c38411fd81c",
+    "url": "/first-react-app-basics/static/js/main.f477699b.chunk.js"
+  },
+  {
+    "revision": "33c309ba0816b0999ffd",
+    "url": "/first-react-app-basics/static/js/runtime-main.cf4f5bf7.js"
+  }
+]);
